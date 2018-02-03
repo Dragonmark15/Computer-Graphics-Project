a0221765f73c7f494b0abe1f7b6e5b40b12f786d
Mark W Linux Drive <weinh030@d.umn.edu>
2018-02-03 17:40:06 -0600

    Added the basis for XML file parsing

--- raytracer/Scene.cpp
+++ raytracer/Scene.cpp
@@ -6,6 +6,8 @@
 #include "Vector3D.h"
 #include "camera.h"
 #include "sphere.h"
+#include "XMLSceneParser.h"
+#include "SceneFile_Instancer.h"
 
 using namespace sivelab;
 
@@ -13,7 +15,8 @@ int main(int argc, char *argv[])
 {
 	GraphicsArgs args;
  	args.process(argc, argv);
-	Vector3D cameraPosition(0,0,0);
+//Hard-Coded data
+/*	Vector3D cameraPosition(0,0,0);
 	Vector3D cameraDirection(0,0,-1);
 	Vector3D sphereCenter(0,0,-5);
 	Vector3D sphereColor(0,0,255); //Completely blue based on PNG colors
@@ -24,22 +27,44 @@ int main(int argc, char *argv[])
 					  0.5,
 					  args.width,
 					  args.height);
-	Sphere sphere0(sphereCenter, 1.0, sphereColor);
-	//
-	// Create a red sphere
-	//
-	//int w = args.width, h = args.height;
+	Sphere sphere0(sphereCenter, 1.0, sphereColor);*/
+
+	//Begin data parse
+	XMLSceneParser xmlScene;
+
+	SceneFile_Instancer *sceneFactory = new SceneFile_Instancer();
+
+	// register object creation function with xmlScene
+	xmlScene.registerCallback("camera", sceneFactory);
+	//xmlScene.registerCallback("light", sceneFactory); //TODO:Pulled for now, since lights haven't been implimented
+	xmlScene.registerCallback("shader", sceneFactory);
+	xmlScene.registerCallback("shape", sceneFactory);
+
+	if (args.inputFileName != "")
+		xmlScene.parseFile( args.inputFileName );
+	else
+	{
+		std::cerr << "Need input file!" << std::endl;
+		exit(EXIT_FAILURE);
+	}
+
+	sceneFactory->instance(           );
+
+
+
+
+
 	png::image< png::rgb_pixel > imData( mainCamera.getPixelWidth(), mainCamera.getPixelHeight() );
-	Vector3D sceneColor;
-	float dummyTMax; //For one sphere, this does nothing, but is still required
+	Vector3D pixelColor;
+	float dummyTMax;
 	for (size_t y = 0; y < imData.get_height(); ++y)
 	{
 		for (size_t x = 0; x < imData.get_width(); ++x)
 		{
-			sceneColor.set(50,50,50);//Gray color if the ray misses
+			pixelColor.set(args.bgColor);
 			dummyTMax = 1000;
-			sphere0.intersect(mainCamera.getPosition(),mainCamera.genRay(x,y), mainCamera.getFocalLength(), dummyTMax, sceneColor);
-			imData[y][x] = png::rgb_pixel(sceneColor[0], sceneColor[1], sceneColor[2]);
+			sphere0.intersect(mainCamera.getPosition(),mainCamera.genRay(x,y), mainCamera.getFocalLength(), dummyTMax, pixelColor);
+			imData[y][x] = png::rgb_pixel(pixelColor[0], pixelColor[1], pixelColor[2]);
 
 			/*if(sphere0.intersect(mainCamera.getPosition(),mainCamera.genRay(x,y)))
 			{
@@ -48,6 +73,6 @@ int main(int argc, char *argv[])
 			else imData[y][x] = png::rgb_pixel(100, 100, 100);*/
 		}
 	}
-	imData.write( "Initial_Sphere_Test.png" );
+	if(args.outputFileName != "") imData.write( args.outputFileName );
+	else imData.write( "Untitled" );
 }
-
