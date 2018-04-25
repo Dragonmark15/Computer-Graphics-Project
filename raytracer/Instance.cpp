6179fabf9f7bffa4ad06b8231fdc03bdacbfa91e
Mark Weinhold <weinh030@d.umn.edu>
2018-04-25 13:37:11 -0500

    Added the base for instancing. Need to add support in Scene.cpp and XML files to test it

---
+++ raytracer/Instance.cpp
@@ -0,0 +1,28 @@
+#include "Instance.h"
+
+Instance::Instance(Matrix4x4 inputMinv, Shape* inputOriginal, Shader* inputShader) {
+	Minv = inputMinv;
+	original = inputOriginal;
+	hit.shader = inputShader;
+}
+
+void Instance::intersect(const Ray rayIn, float tMin, float &tMax, HitStructure &inputHit) {
+	Ray instanceRay;
+	float oldTMax = tMax; //Used to determine if the shader should override. If tMax changes, it hit
+	double w = 1;
+	instanceRay.origin = Minv.multVector(rayIn.origin, w);
+	w = 1;
+	instanceRay.direction = Minv.multVector(rayIn.direction, w);
+	original->intersect(instanceRay, tMin, tMax, inputHit);
+	if (oldTMax != tMax)
+		inputHit.shader = hit.shader;
+}
+
+bool Instance::intersect(const Ray rayIn) {
+	Ray instanceRay;
+	double w = 1;
+	instanceRay.origin = Minv.multVector(rayIn.origin, w);
+	w = 1;
+	instanceRay.direction = Minv.multVector(rayIn.direction, w);
+	return original->intersect(instanceRay);
+}
---
+++ raytracer/Instance.h
@@ -0,0 +1,24 @@
+#ifndef __INSTANCE_H__
+#define __INSTANCE_H__
+
+#include "Vector3D.h"
+#include "Matrix4x4.h"
+#include "Shape.h"
+#include "Shader.h"
+
+using namespace sivelab;
+
+class Instance : public Shape {
+public:
+	~Instance(){}
+	Instance(Matrix4x4 inputMinv, Shape* inputOriginal, Shader* inputShader);
+
+	void intersect(const Ray rayIn, float tMin, float &tMax, HitStructure &hit);
+	bool intersect(const Ray rayIn);
+
+private:
+	Matrix4x4 Minv;
+	Shape* original;
+};
+
+#endif
